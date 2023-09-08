41967fc305162c3880c30c50cb8a1a286ca031b5
Ashutosh Singh <ashutoshs.ug21.cs@nitp.ac.in>
2023-09-08 22:00:03 +0530

    Folder Setup

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-import Hero from '@/components/Hero'
+import React from 'react'
 
-export default function Home() {
+export default function Hero() {
   return (
-    <div>
-      <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+    <div className="hero" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
@@ -14,7 +12,5 @@ export default function Home() {
     </div>
   </div>
 </div>
-    </div>
-    
   )
 }
